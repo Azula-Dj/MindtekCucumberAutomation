08b8a4afe6c4b8a5d4a5057b32c7ca44091a5325
Azula-Dj <azulafire1377@gmail.com>
2022-04-13 20:31:17 -0500

    Added hasCar and catName.Deleted salary

--- src/test/java/steps/Ainura.java
+++ src/test/java/steps/Ainura.java
@@ -4,6 +4,9 @@ public class Ainura {
     public static void main(String[] args) {
         String name="Ainura";
         int age=23;
-        double salary=100.10;
+        boolean hasCar=false;
+        String catName="Misha";
+
+
     }
 }
